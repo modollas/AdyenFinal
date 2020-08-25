1af429a5f9b94ea50045c5a429cb515e264ed1f6
Rahul Vig <rvig@OMSC>
2020-08-25 14:26:28 -0400

    added hero

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ app.use(bodyParser.json());
 // Parse cookie bodies, and allow setting/getting cookies
 // app.use(cookieParser());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://intense-waters-87955.herokuapp.com'
+  );
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
